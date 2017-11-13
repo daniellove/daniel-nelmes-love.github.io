3c7e610cd8510b2c7adb3fe4cbd659a80dd208d2
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2017-11-13 22:04:12 +1300

    New portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,7 @@
-var scrollSpeed = 800
-var fadeSpeed = 400
-
-function aboutMe() {
-	var toAboutMe = $('#mastHead').outerHeight(true)
-	$('html, body').animate({scrollTop: toAboutMe}, scrollSpeed);
-}
-
-function openProjects() {
-	$('#projects').fadeIn(fadeSpeed)
-	$('body').css('overflow', 'hidden')
-}
-
-function closeProjects() {
-	$('#projects').fadeOut(fadeSpeed)
-	$('body').css('overflow', 'initial')
-}
-
-$(document).ready(function() {
-	$('body').append('\
+$(window).on('load',function() {
+	$('body')
+		.addClass('docLoading')
+		.append('\
 		<div class="loading">\
 			<div>\
 				<img src="img/gears.gif">\
@@ -27,45 +11,7 @@ $(document).ready(function() {
 	')
 })
 
-$(window).on('load', function() {
-
-	$('.loading').remove()
-	$('body').css('overflow', 'initial')
-
-	setUpProjects()
-
-	$('.project').hover(function() {
-		$(this).find('.caption').stop().animate({
-			'top': 0
-		}, fadeSpeed)
-	}, function() {
-		$(this).find('.caption').stop().animate({
-			'top': '100%'
-		}, fadeSpeed)
-	})
-
-	$('.overlay').on('click', function() {
-		closeProjects()
-	})
+$(function() {
+	$(document).find('.loading').remove()
+	$('body').removeClass('docLoading')
 })
-
-function setUpProjects() {
-
-	setTimeout(function() {
-		$('#projects').css({
-			'display': 'none',
-			'opacity': 'initial'
-		})
-	}, 100)
-
-	var containerHeight = $('#projects .container').outerHeight(true)
-	var headerHeight = $('#projects .header').outerHeight(true)
-	var projectSpace = $('#projects .viewer').outerHeight(true)
-		projectHeight = projectSpace - $('#projects .viewer').outerHeight(false)
-	$('#projects .viewer').css('height', containerHeight-(headerHeight+projectHeight))
-	$('.project').each(function() {
-		var imageHeight = $(this).find('img').outerHeight(true)
-		$(this).css('height', imageHeight)
-	})
-	
-}
